e38d12f8f81e8f25b75b80b54b820210c0a227dc
AmitaiBiton <64432837+AmitaiBiton@users.noreply.github.com>
2021-07-17 20:56:04 +0300

    update file  - get the position image from the first scan and render to the scan scan by RT matrix

--- localization_processes.py
+++ localization_processes.py
@@ -6,7 +6,9 @@ from detect_Qr_code import detect_qr_code
 from pipe_detected_by_image_processing import read_aligned_frames
 from streaming_from_bag_file import streaming_from_bag_file
 import cv2
+import math
 import  numpy as np
+import torch
 """
 step 1: build reference points
 build points that we detected on qr code  
@@ -49,7 +51,6 @@ def build_reference_points(point1,point2):
     point_list2.append((point2[0][0] + 2, point2[0][1] + 2))
     return  point_list1,point_list2
 
-
 def add_vector_one(list):
     new_list=[]
     for i in range(list.shape[0]):
@@ -62,40 +63,59 @@ def add_vector_one(list):
         new_list.append(t1)
     return new_list
 
-
 def pixel_uv_to_points_xyz(list):
     points=[]
     for i in range(len(list)):
         udist1 = depth_data_array[list[i][0], list[i][1]]
-        points.append(rs.rs2_deproject_pixel_to_point(color_intrin, [list[i][0], list[i][1]], udist1))
+        #udist1  = depth_frame.get_distance(list[i][0], list[i][1])
+        points.append(rs.rs2_deproject_pixel_to_point(depth_intrin, [list[i][0], list[i][1]], udist1))
     return points
 
-def get_pixel_pipes():
+def get_pixel_pipes(image):
     list_pixel_pipes=[]
-    tracking = LineTracking('./output/new5.jpg')
+
+    tracking = LineTracking(image)
     tracking.processing()
     tracking.img_final = tracking.img_final[:, :, 0]
     for i in range(tracking.img_final.shape[0]):
         for j in range(tracking.img_final.shape[1]):
             if tracking.img_final[i][j] > 0:
-                list_pixel_pipes.append((i, j))
+                list_pixel_pipes.append((image1[i][j]))
     return list_pixel_pipes
 
+def image_from_2D_to_3D(image):
+    list_image = []
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            udist1 = depth_data_array[i,j]
+            # udist1  = depth_frame.get_distance(list[i][0], list[i][1])
+            list_image.append(rs.rs2_deproject_pixel_to_point(color_intrin, [j,i], udist1))
+    return  list_image
 
-streaming_from_bag_file('./scaning/obj5.bag' ,'./output/new6.jpg' )
-image1 = cv2.imread('./output/new6.jpg')
-streaming_from_bag_file('./scaning_2/obj5.bag' ,'./output_2/new6.jpg' )
-image2 = cv2.imread('./output_2/new6.jpg')
-image_QR_detector1 , point1 = detect_qr_code(image1)
-image_QR_detector2 , point2 = detect_qr_code(image2)
+device = "cpu"
+if (torch.cuda.is_available()):
+    device = "cuda"
+print(device)
+image1 = streaming_from_bag_file('./pipe_finder/salon/1.bag')
+image2 = streaming_from_bag_file('./pipe_finder/salon/2.bag')
 
-point_list1 ,point_list2 = build_reference_points(point1,point2)
 
+img2 = np.copy(image2)
+#image_QR_detector1 , point1 = detect_qr_code(image1)
+#image_QR_detector2 , point2 = detect_qr_code(image2)
+
+#point_list1 ,point_list2 = build_reference_points(point1,point2)
+point_list1 = [[248,676],[367,675],[367,798],[248,795]]
+point_list2 = [[246,674],[365,676],[365,798],[246,795]]
 aligned_depth_frame, aligned_frames = read_aligned_frames('./scaning/obj5.bag')
 color_frame = aligned_frames.get_color_frame()
-
+depth_frame = aligned_frames.get_depth_frame()
 depth_data_array = np.asanyarray(aligned_depth_frame.get_data())
 color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
+depth_intrin = depth_frame.profile.as_video_stream_profile().intrinsics
+
+depth_to_color_extrin = depth_frame.profile.get_extrinsics_to(color_frame.profile)
+
 
 points1 = pixel_uv_to_points_xyz(point_list1)
 points2 = pixel_uv_to_points_xyz(point_list2)
@@ -106,19 +126,64 @@ points2 = np.array(points2)
 
 p1 = add_vector_one(points1)
 p2 = add_vector_one((points2))
-matrix_A  = np.array(p1)
-matrix_B = np.array(p2)
-print(matrix_A)
-print(matrix_B)
-matrix_A_invers = inv(matrix_A)
-matrix_RT = np.dot(matrix_A_invers , matrix_B)
-print(matrix_RT)
+#p1=np.hstack((points1,np.ones((points1.shape[0],1))))
+A = np.array(p1)
+B = np.array(p2)
+#
 
-list_pixel_pipes = get_pixel_pipes()
-list_point_pipes = []
+#matrix_RT = np.dot(matrix_A_invers,B)
+matrix_RT = np.float64(np.dot(B.T,inv(A.T)))
 
-list_point_pipes = pixel_uv_to_points_xyz(list_pixel_pipes)
-list_point_pipes = np.array(list_point_pipes)
-points_pipes_3D = add_vector_one(list_point_pipes)
+points_image_A = image_from_2D_to_3D(image1)
+#points_image_B = image_from_2D_to_3D(image2)
 
-#print(points_pipes_3D[0] , list_point_pipes[0])
+points_image_A = np.array(points_image_A)
+points_image_A = add_vector_one(points_image_A)
+
+points_image_A = np.array(points_image_A)
+image1_position_image2 = np.dot(matrix_RT,points_image_A.T)
+
+final_pixel_projection = []
+for row in image1_position_image2.T:
+    point = row[:3]
+    #color_point = rs.rs2_transform_point_to_point(depth_to_color_extrin, depth_point)
+    color_pixel = rs.rs2_project_point_to_pixel(color_intrin, point)
+    if  math.isnan(color_pixel[0])!= True and math.isnan(color_pixel[1])!= True:
+        final_pixel_projection.append((int(color_pixel[0]), int(color_pixel[1])))
+
+final_pixel_projection = np.array(final_pixel_projection)
+list_value_color = []
+for i in range(image1.shape[0]):
+    for j in range(image1.shape[1]):
+        list_value_color.append((image1[i][j]))
+
+
+final_pixel_projection = np.array(final_pixel_projection)
+
+list_value_color = np.reshape(list_value_color, (1080, 1920,3))
+final_pixel_projection = np.reshape(final_pixel_projection, (1080, 1920,2))
+
+new_color_image_localizatiion = np.copy(image2)
+new_color_image_localizatiion[:, :, :] = 155
+
+for i in range(final_pixel_projection.shape[0]):
+    for j in range(final_pixel_projection.shape[1]):
+        if final_pixel_projection[i][j][0] < 0:
+            final_pixel_projection[i][j][0] = 0
+        if final_pixel_projection[i][j][1] < 0:
+            final_pixel_projection[i][j][1] = 0
+        if final_pixel_projection[i][j][0] >= 1800:
+            final_pixel_projection[i][j][0] = 0
+        if final_pixel_projection[i][j][1] >= 1080:
+            final_pixel_projection[i][j][1] = 0
+image  = cv2.imread('./pipe_finder/salon/1.jpg')
+list_pixel_pipes = get_pixel_pipes(image)
+print(list_pixel_pipes)
+color_exist = False
+for i in range(list_value_color.shape[0]):
+    for j in range(list_value_color.shape[1]):
+       image2[i][j]= list_value_color[final_pixel_projection[i][j][1]][final_pixel_projection[i][j][0]]
+
+cv2.imshow("new imaghe" , image2)
+cv2.waitKey()
+
